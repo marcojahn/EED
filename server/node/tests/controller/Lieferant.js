f6f60a93ffc63887b7bfa5935727afdff269f770
Marco Jahn <marco.jahn@gmail.com>
2013-03-13 09:40:29 +0100

    phpstorm 5 -> 6

--- server/node/tests/controller/Lieferant.js
+++ server/node/tests/controller/Lieferant.js
@@ -3,7 +3,6 @@ var vows = require('vows'),
     assert = require('assert'),
     express = require('express'),
     mongoose = require('mongoose'),
-    LieferantModel = require('../../model/Lieferant.js'),
     LieferantController = require('../../controller/Lieferant.js');
 
 var result = {
